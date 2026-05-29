676b0da46b4196d3005aa763c7c3b95bb8489245
Shubham Waghmare <scwagh123@gmail.com>
2026-05-29 11:19:41 +0530

    package and modules

---
+++ demo_venv/mypackage/module1.py
@@ -0,0 +1,13 @@
+def addition(*args):
+    return sum(args)
+
+def double(num):
+    return num* 2        
+
+def greet(name):
+    return f"Hello {name}"
+
+x = 30
+y = 20
+
+z = 100
---
+++ demo_venv/mypackage/module2.py
@@ -0,0 +1,7 @@
+def minus(a,b):
+    return a - b
+
+
+x = 100
+y = 50
+z = 100
